6783422283ef69a1ba80811c91c8de398e014879
Julie Kuehl <julie@juliekuehl.com>
2015-03-18 17:57:44 -0500

    friggin' mobile menu sorted

--- header.php
+++ header.php
@@ -22,63 +22,60 @@
 <!-- HEADER IMAGE -->
 <?php global $post; ?>
 <?php $src = wp_get_attachment_image_src( get_post_thumbnail_id($post->ID), 'large', false ); ?>
-	<div class="page-header-image" style="background: #bdd4de url(<?php echo $src[0]; ?> ) no-repeat center center !important;">
+<div class="page-header-image" style="background: #bdd4de url(<?php echo $src[0]; ?> ) no-repeat center center !important;">
 
-		<!--- NAV BAR --->
-		<nav class="navbar navbar-inverse navbar-fixed-top" id="floating-navbar" role="navigation">
-			<div class="container">
-				<div class="color-bar">
-					<div class="navbar-header">
+	<!--- NAV BAR --->
+	<nav class="navbar navbar-inverse navbar-fixed-top" id="floating-navbar" role="navigation">
+		<div class="container">
+			<div class="color-bar">
+				<div class="navbar-header">
 
-						<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png" title="Market Cadence" class="site-logo" rel="home" /></a>
-					</div><!-- .navbar-header -->
+					<a class="navbar-brand" href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><img src="<?php echo get_stylesheet_directory_uri(); ?>/images/logo.png" title="Market Cadence" class="site-logo" rel="home" /></a>
+				</div><!-- .navbar-header -->
 
-					<div class="navbar-group">
+				<div class="navbar-group">
 
-						<div class="social-icons-box">
-							<?php if ( dynamic_sidebar( 'social-icons-box' ) ); ?>
-						</div><!-- .social-icons-box -->
+					<div class="social-icons-box">
+						<?php if ( dynamic_sidebar( 'social-icons-box' ) ); ?>
+					</div><!-- .social-icons-box -->
 
-						<button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
-							<span class="sr-only">Toggle navigation</span>
-							<img src="<?php echo get_stylesheet_directory_uri(); ?>/images/menu.png" title="Menu icon" />
-						</button>
+					<button class="btn btn-default" id="menu-button" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
+						<span class="sr-only">Toggle navigation</span>
+						<div class="menu-bar-button">
+							MENU <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/menu.png" title="Menu icon" />
+						</div><!-- .menu-bar-button -->
+					</button>
 
-						<button class="btn btn-default" id="menu-button" type="button" data-toggle="collapse" data-target="#collapseExample" aria-expanded="false" aria-controls="collapseExample">
-							<div class="menu-bar-button">
-								MENU <img src="<?php echo get_stylesheet_directory_uri(); ?>/images/menu.png" title="Menu icon" />
-							</div><!-- .menu-bar-button -->
-						</button>
-
-					</div><!-- .navbar-group -->
-
-				</div><!-- .color-bar -->
-
-			</div><!-- .container -->
-
-			<div class="container" id="well-menu">
-					<div class="collapse" id="collapseExample">
-						<div class="well">
-
-							<div id="dropped-navbar" class="navbar-collapse collapse">
-
-									<?php
-										$args = array(
-											'menu'          => 'header-menu',
-											'menu_class'    => 'nav navbar-nav',
-											'container'     => 'false',
-										);
-										wp_nav_menu( $args );
-									?>
-
-							</div><!-- .navbar-collapse #dropped-navbar -->
-
-						</div><!-- .well -->
-					</div><!-- .collapse -->
 				</div><!-- .navbar-group -->
-			</div><!-- .container #well-menu -->
-		</nav>
-	</div><!-- .page-header-image -->
+
+			</div><!-- .color-bar -->
+
+		</div><!-- .container -->
+
+		<div class="container" id="well-menu">
+			<div class="collapse" id="collapseExample">
+				<div class="well">
+
+					<div id="dropped-navbar" class="navbar-collapse collapse">
+
+						<?php
+						$args = array(
+							'menu'          => 'header-menu',
+							'menu_class'    => 'nav navbar-nav',
+							'container'     => 'false',
+						);
+						wp_nav_menu( $args );
+						?>
+
+					</div><!-- .navbar-collapse #dropped-navbar -->
+
+				</div><!-- .well -->
+			</div><!-- .collapse -->
+		</div><!-- .navbar-group -->
+</div><!-- .container #well-menu -->
+</nav>
+
+</div><!-- .page-header-image -->
 
 
 <div id="content">
